db9fc011fb1aa78029715efd8c4201dd7a3926b4
Rob Woodward <rob@emailplus.org>
2023-04-24 16:53:30 +0100

    More fixes

--- js/close_delete.js
+++ js/close_delete.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+document.addEventListener("DOMContentLoaded", () => {
+  (document.querySelectorAll(".notification .delete") || []).forEach($delete => {
     const $notification = $delete.parentNode;
 
-    $delete.addEventListener('click', () => {
+    $delete.addEventListener("click", () => {
       $notification.parentNode.removeChild($notification);
     });
   });
